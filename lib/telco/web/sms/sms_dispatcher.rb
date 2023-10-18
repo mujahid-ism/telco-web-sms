052721f2d47871dfcae1b2a4a80c9665a190cf08
mujahid-ism <mujahidul.islam@selise.ch>
2023-10-18 18:52:30 +0600

    Test

--- lib/telco/web/sms/sms_dispatcher.rb
+++ lib/telco/web/sms/sms_dispatcher.rb
@@ -24,17 +24,11 @@ module Telco
 
         def client
           Faraday.new(Configuration.default.web_sms_url) do |connection|
-            # connection.adapter Faraday::Request::UrlEncoded
-            # connection.adapter Faraday::Response::RaiseError
-            # connection.adapter Faraday::Adapter::NetHttp
-            # connection.response :detailed_logger, Rails.logger, "web-sms"
-            # connection.request :json, content_type: "application/json"
-
             connection.request :url_encoded
-            connection.response :raise_error
-            connection.adapter :net_http
-            connection.use FaradayMiddleware::EncodeJson, content_type: "application/json"
+            connection.adapter Faraday::Response::RaiseError
+            connection.adapter Faraday::Adapter::NetHttp
             connection.response :detailed_logger, Rails.logger, "web-sms"
+            connection.request :json, content_type: "application/json"
           end
         end
 
