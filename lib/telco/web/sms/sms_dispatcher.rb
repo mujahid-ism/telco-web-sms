aa58551f490c0aed7875eca4be6c7dda769397d5
mujahid-ism <mujahidul.islam@selise.ch>
2023-10-18 13:46:40 +0600

    test

--- lib/telco/web/sms/sms_dispatcher.rb
+++ lib/telco/web/sms/sms_dispatcher.rb
@@ -24,23 +24,11 @@ module Telco
 
         def client
           Faraday.new(Configuration.default.web_sms_url) do |connection|
-            # connection.adapter Faraday::Request::UrlEncoded
-            # connection.adapter Faraday::Response::RaiseError
-            # connection.adapter Faraday::Adapter::NetHttp
-            # connection.response :detailed_logger, Rails.logger, "web-sms"
-            # connection.request :json, content_type: "application/json"
-
-            connection.use Faraday::Request::UrlEncoded # To encode the request as URL-encoded form data
-            connection.use Faraday::Response::RaiseError # To raise exceptions on HTTP error responses
-            connection.use Faraday::Adapter::NetHttp # To use the Net::HTTP adapter for making HTTP requests
-
-            # If you want to log the requests and responses, you can define a middleware for it
-            connection.response :logger, Rails.logger, headers: true, bodies: true
-
-            connection.request :json # To send requests as JSON
-            connection.headers["Content-Type"] = "application/json" # Set the content type header for JSON
-
-            # Add any additional middlewares or configurations you need here.
+            connection.use Faraday::Request::UrlEncoded
+            connection.use Faraday::Response::RaiseError
+            connection.use Faraday::Adapter::NetHttp
+            connection.response :detailed_logger, Rails.logger, "web-sms"
+            connection.request :json, content_type: "application/json"
           end
         end
 
